c37f5b3b05cdc2b02a9e9deb1016eec7edf0915d
Bakare OLuwatobiloba <bakareoluwatobiloba13@gmail.com>
2023-04-25 12:04:02 +0000

    trofy

--- auctions/static/auctions/load_products.js
+++ auctions/static/auctions/load_products.js
@@ -1,27 +1,5 @@
 document.addEventListener('DOMContentLoaded',function () {
-    carousel_ids = randomNum(1,17)
-    fetch(`https://api.escuelajs.co/api/v1/products?offset=0&limit=3`)
-            .then(res=>res.json())
-            .then(items => {
-                items.forEach(function(item){
-                     console.log(item,carousel_ids)
-                carousel_div = document.createElement('div')
-                carousel_div.className = 'carousel_container'
-                carousel_div.innerHTML = `<a >
-                <img src="${item.images[0]}" alt="" class="col-md"id="carousel_image" ><br>
-                ${item.title}
-                <br> Starting price:${item.price} 
-            </a>`
-/*         document.querySelector('#carousel').appendChild(carousel_div)
- */                })
-               
     
- 
-        
-
-                
-                
-        })
     fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=20')
     .then(res=>res.json())
     .then(products => {
@@ -35,7 +13,7 @@ document.addEventListener('DOMContentLoaded',function () {
            ${product.title}
             <br> Starting price:$${product.price} 
         </a>`
-        document.querySelector('#products').appendChild(product_container)
+         document.querySelector('#products').appendChild(product_container)
         
 
         })
@@ -47,7 +25,7 @@ document.addEventListener('DOMContentLoaded',function () {
 
         var elements = document.querySelectorAll('.listings');
         elements.forEach(function(element) {
-            element.classList.add('card','text-center',); //add class
+            element.classList.add('card','text-center','col-3','p-2','m-1','mb-4'); //add class
         });
 
 
@@ -56,10 +34,3 @@ document.addEventListener('DOMContentLoaded',function () {
 
 })
 
-function randomNum(min, max) { 
-    var n = []; 
-    for(var i=0;i<1;i++){ 
-    n.push(Math.floor(Math.random() * max) + min); 
-    } 
-    return n; 
-    } 
--- commerce/settings.py
+++ commerce/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    } 
 }
 
 AUTH_USER_MODEL = 'auctions.User'
